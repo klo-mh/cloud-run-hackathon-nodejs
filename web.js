f55c5276fb783263d1a3b31f89119109317e61da
Kenny Lo <kenny.lo@mightyhive.com>
2022-09-09 11:05:18 +1000

    test

--- web.js
+++ web.js
@@ -9,12 +9,27 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-  console.log(req.body);
+  // console.log(req.body);
+  const info = req.body;
   const moves = ['F', 'T', 'L', 'R'];
 
   // TODO add your implementation here to replace the random response
-  
+  let lowestScorePlayer = findLowestScorePlayer(info);
+  console.log('LOWEST PLAYER');
+  console.log(lowestScorePlayer);
   res.send(moves[1]);
+
+  let findLowestScorePlayer = function(info){
+    let arena = info.arena;
+    let state = arena.state;
+    let lowestScorePlayer = state[0];
+    for(let i=0;i<state.length;i++){
+      console.log(state[i]);
+      if (state[i].score < lowestScorePlayer.score){
+        lowestScorePlayer = state[i];
+      }
+    }
+  }
 });
 
 app.listen(process.env.PORT || 8080);
