7b7bbc05ef57a29f28dca2d325e0a6ab275e5297
Kenny Lo <kenny.lo@mightyhive.com>
2022-09-09 11:17:10 +1000

    test

--- web.js
+++ web.js
@@ -9,28 +9,25 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
+  console.log('BODY');
   console.log(req.body);
   const info = req.body;
   const moves = ['F', 'T', 'L', 'R'];
-  let findLowestScorePlayer = function(info){
-
-  }
   // TODO add your implementation here to replace the random response
   // let lowestScorePlayer = findLowestScorePlayer(info);
   let arena = info.arena;
   let state = arena.state;
-  let lowestScorePlayer = state[0];
-  for(let i=0;i<state.length;i++){
-    console.log(state[i]);
-    if (state[i].score < lowestScorePlayer.score){
-      lowestScorePlayer = state[i];
+  let lowestScorePlayer = state["https://cloud-run-hackathon-nodejs-ytkkcthfia-uc.a.run.app"];
+  for (let [key, value] of Object.entries(p)) {
+    console.log("EACH PLAYER");
+    console.log(value);
+    if (value.score < lowestScorePlayer.score) {
+      lowestScorePlayer = value;
     }
   }
   console.log('LOWEST PLAYER');
   console.log(lowestScorePlayer);
   res.send(moves[1]);
-
-
 });
 
 app.listen(process.env.PORT || 8080);
